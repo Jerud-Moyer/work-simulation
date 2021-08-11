dd561d7c220dbb8d268b7da022a475871a916bd0
Jerud-Moyer <jerudmoyer@gmail.com>
2021-08-11 16:04:04 -0700

    add useSelector hook

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -2,7 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { Input, Header, Messages } from "./index";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
+import { selectActiveConversation, selectConversations, selectUser } from "../../store/utils/selectors";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -20,10 +21,13 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-const ActiveChat = (props) => {
+const ActiveChat = () => {
   const classes = useStyles();
-  const { user } = props;
-  const conversation = props.conversation || {};
+  const user  = useSelector(selectUser);
+  const conversations = useSelector(selectConversations);
+  const activeConversation = useSelector(selectActiveConversation);
+  const conversation = conversations.find(
+    (conversation) => conversation.otherUser.username === activeConversation) || {};
 
   return (
     <Box className={classes.root}>
@@ -51,15 +55,15 @@ const ActiveChat = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    conversation:
-      state.conversations &&
-      state.conversations.find(
-        (conversation) => conversation.otherUser.username === state.activeConversation
-      )
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     user: state.user,
+//     conversation:
+//       state.conversations &&
+//       state.conversations.find(
+//         (conversation) => conversation.otherUser.username === state.activeConversation
+//       )
+//   };
+// };
 
-export default connect(mapStateToProps, null)(ActiveChat);
+export default ActiveChat;
